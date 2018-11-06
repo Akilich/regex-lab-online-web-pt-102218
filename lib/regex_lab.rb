0ccb6eca89ab4b53da5a5c3208055cebc74bdb49
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-06 21:48:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-
+"afoot Excellent incredible Absolute unreal Inconceivable octopus".match(/[aeiou]\)
+starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
